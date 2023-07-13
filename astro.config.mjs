797fdc6dc24d043d6961f6800376f7e4c007e89e
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-07-13 10:21:13 -0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  scopedStyleStrategy: "class",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    image(),
+  ],
+});
