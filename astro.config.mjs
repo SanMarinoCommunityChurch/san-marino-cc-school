b972e9c1977f4f63df5bc6e1b1287a1229687c15
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-08-31 10:17:04 -0700

    Update astro.config.mjs
    Changed canonical site path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.smccnurseryschool.com",
+  site: "https://smccnurseryschool.com",
   scopedStyleStrategy: "class",
   integrations: [tailwind({
     applyBaseStyles: false
